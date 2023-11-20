7e7ba971fa8cb9a166fc7466f8dad652e1e9badd
PabloArdila <ardilapabloa@gmail.com>
2023-11-20 18:51:53 -0500

    base de datos conectada a siifo

--- src/main/java/com/siifo/siifo/entity/Producto.java
+++ src/main/java/com/siifo/siifo/entity/Producto.java
@@ -1,5 +1,8 @@
 package com.siifo.siifo.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +17,15 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idProductos;
+    @Column(name = "nombre_productos", nullable = true, columnDefinition = "VARCHAR(250)")
     private String nombreProductos;
+    @Column(name = "precio_compra", nullable = true)
     private double precioCompra;
+    @Column(name = "cantidad", nullable = true)
     private int cantidad;
-    private String fechaEntrega;
+    @Column(name = "fecha_entrega", nullable = true)
+    private Date fechaEntrega;
+    @Column(name = "estado", nullable = true, columnDefinition = "VARCHAR(250)")
     private String estado;
 
 }
