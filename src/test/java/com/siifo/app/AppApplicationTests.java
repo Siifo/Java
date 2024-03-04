114b7ad60c6f4048ac9581f4826c56f8e63069bf
alexander <alexiscastaneda226@gmail.com>
2024-03-04 16:39:22 -0500

    arquitectura

---
+++ src/test/java/com/siifo/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siifo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
